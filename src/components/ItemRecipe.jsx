cabcf3f59f4acb2d6628435b59317635b9cf6c26
Stu Bolderson <stu.bolderson@aol.com>
2022-03-10 23:14:35 +0100

    images added - img size reduced even further but need to look at each img and replace if needed

--- src/components/ItemRecipe.jsx
+++ src/components/ItemRecipe.jsx
@@ -1,8 +1,15 @@
 export default function ItemRecipe({ item }) {
-  const { title, body } = item;
+  console.log(item);
+  const { title, body, thumbnailImage } = item;
+
   return (
     <div>
-      <img src="" alt="coming soon" width={50} height={50} />
+      <img
+        src={require(`../assets/images/recipes/${thumbnailImage}`)}
+        alt={title}
+        height={400}
+        width={400}
+      />
       <h2>{title}</h2>
       <p>{body}</p>
     </div>
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,13 +2,21 @@
 import { Link } from "react-router-dom";
 
 export default function MenuItem({ item }) {
-  const { title, body, slug } = item;
+  const { title, body, slug, thumbnailImage } = item;
   return (
     <div>
       <Link to={`/product/${slug}`}>
         <h2>{title}</h2>
         <p>{body}</p>
-        <img src="" alt="coming soon.." />
+        <div style={{ height: "200px", width: "200px" }}>
+          <img
+            src={require(`../assets/images/recipes/${thumbnailImage}`)}
+            alt={title}
+            style={{ objectFit: "cover" }}
+            height={"100%"}
+            width={"100%"}
+          />
+        </div>
       </Link>
     </div>
   );
